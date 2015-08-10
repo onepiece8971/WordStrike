b4056f194371aa221188cb0edae6df12d9f65aad
chenglinz <onepiece8971@163.com>
2015-08-10 20:49:06 +0800

    create plugins

--- wp-content/plugins/wordstrike/wordStrike.php
+++ wp-content/plugins/wordstrike/wordStrike.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @package wordsstrike
+ * @package wordStrike
  */
 /*
-Plugin Name: wordsstrike
+Plugin Name: wordStrike
 Version: 1.0.0
 Author: chenglinz
 License: GPLv2 or later
@@ -11,7 +11,7 @@ Text Domain: wordsstrike
 */
 define( 'WORDSTRIKE_DIR', plugin_dir_path( __FILE__ ) );
 
-require_once( WORDSTRIKE_DIR . 'class.wordsstrike.php' );
+require_once( WORDSTRIKE_DIR . 'class.wordStrike.php' );
 
 //Active
-register_activation_hook(__FILE__, array('wordsstrike' , 'init'));
+register_activation_hook(__FILE__, array('wordStrike' , 'init'));
