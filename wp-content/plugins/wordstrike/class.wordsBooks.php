80efb389d8d25d0f405b96946ea766c81833e6ae
chenglinz <onepiece8971@163.com>
2015-08-25 18:43:02 +0800

    wordsbooks页面

---
+++ wp-content/plugins/wordstrike/class.wordsBooks.php
@@ -0,0 +1,37 @@
+<?php
+class wordsBooks
+{
+    public function getWordsBooks()
+    {
+        global $wpdb;
+        $query = "SELECT id, name FROM ".Wordstrike::$table_prefix."words_books where act = 1 ORDER BY create_time desc";
+        return $wpdb->get_results($query, ARRAY_A);
+    }
+
+    /**
+     * 判断用户是否已添加相应生词本
+     *
+     * @param $uid
+     * @param $bookId
+     * @return bool
+     */
+    public function isMyWordsBook($uid,  $bookId)
+    {
+        global $wpdb;
+        $query = "SELECT * FROM ".Wordstrike::$table_prefix."user_words_books where act = 1 and books_id = ".$bookId." and uid = ".$uid;
+        $row = $wpdb->get_row($query);
+        return empty($row);
+    }
+}
+
+function getWordsBooks()
+{
+    $wordsBooks = new wordsBooks;
+    return $wordsBooks->getWordsBooks();
+}
+
+function isMyWordsBook($uid, $bookId)
+{
+    $wordsBooks = new wordsBooks;
+    return $wordsBooks->isMyWordsBook($uid, $bookId);
+}
