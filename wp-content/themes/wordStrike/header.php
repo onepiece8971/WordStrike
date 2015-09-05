cbca16b956a24ac1221996b94abcd6bc7b91ff2b
chenglinz <onepiece8971@163.com>
2015-09-05 14:26:26 +0800

    增加音标字体

--- wp-content/themes/wordStrike/header.php
+++ wp-content/themes/wordStrike/header.php
@@ -1,5 +1,6 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
     <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0" name="viewport">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
     <link rel="stylesheet" type="text/css" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/semantic.css">
